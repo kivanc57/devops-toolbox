358ac74f97ecd8d9a769290e1b91390685709fb7
Kivanc Gordu <kivancgordu@hotmail.com>
2026-07-30 17:07:13 +0200

    fix(backup): correct backup scripts

--- backup/disk-backup/disk-backup.sh
+++ backup/disk-backup/disk-backup.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 SOURCE_DISK="/dev/nvme0n1p1"
-BACKUP_FOLDER="${HOME}/backups"
-BACKUP_ARCHIVE="${BACKUPS_DIR}/disk-$(date +%Y-%m-%d).img.gz"
+BACKUP_PATH="${HOME}/backups"
+BACKUP_ARCHIVE="${BACKUP_PATH}/disk-$(date +%Y-%m-%d).img.gz"
 
-mkdir -p "${BACKUP_FOLDER}"
+mkdir -p "${BACKUP_PATH}"
 
-dd if="${SOURCE_DISK}" bs=4M status=progress | gzip > "${BACKUP_ARCHIVE}"
+sudo dd if="${SOURCE_DISK}" bs=4M status=progress | gzip > "${BACKUP_ARCHIVE}"
 
--- backup/folder-backup/folder-backup.sh
+++ backup/folder-backup/folder-backup.sh
@@ -6,17 +6,15 @@ source .env
 : "${SV_USERNAME:?SV_USERNAME not set}"
 : "${SV_IP:?SV_IP not set}"
 
-export SV_USERNAME
-export SV_IP
-
-BACKUP_FOLDER="${HOME}/backups"
-SYNC_FOLDER=${HOME}
-DEST_FOLDER="${BACKUP_FOLDER}/${SYNC_FOLDER}-$(date +%Y-%m-%d)"
+SYNC_FOLDER="${HOME}"
+DEST_FOLDER="backups/$(date +%Y-%m-%d)"
 EXCLUDED="excluded.txt"
 
-mkdir -p "${BACKUP_FOLDER}"
-
-rsync -azP --delete --exclude-from="${EXCLUDED}" \
-    --no-owner --no-group --ignore-errors \
-    "${SYNC_FOLDER}" "${SV_USERNAME}@${SV_IP}:${DEST_FOLDER}"
+rsync -azP \
+    --mkpath \
+    --exclude-from="${EXCLUDED}" \
+    --no-owner \
+    --no-group \
+    "${SYNC_FOLDER}/" \
+    "${SV_USERNAME}@${SV_IP}:${DEST_FOLDER}/"
 
